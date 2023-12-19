2b346088ca8485a4c9fb89cfe9d209ce64f71103
Nguyen Truong <xuantruong200101@gmai.com>
2023-12-19 14:26:26 +0700

    dev

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,9 +18,18 @@ import YAML from 'yaml'
 import fs from 'fs'
 import path from 'path'
 import swaggerUi from 'swagger-ui-express'
-import { envConfig } from './constants/config'
+import { envConfig, isProduction } from './constants/config'
+import helmet from 'helmet'
+import { rateLimit } from 'express-rate-limit'
 const file = fs.readFileSync(path.resolve('Twitter_Swagger.yaml'), 'utf8')
 const swaggerDocument = YAML.parse(file)
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers.
+})
 
 config()
 databaseService.connect().then(() => {
@@ -33,10 +42,13 @@ databaseService.connect().then(() => {
 initFolder()
 const app = express()
 const httpServer = createServer(app)
-
 const port = envConfig.port || 4000
+const corsOption = {
+  origin: isProduction ? envConfig.clientUrl : '*'
+}
+app.use(helmet())
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOption))
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
@@ -51,5 +63,5 @@ app.use(defaultErrorHandler)
 initSocket(httpServer)
 
 httpServer.listen(port, () => {
-  console.log(`Example app listening onport ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
